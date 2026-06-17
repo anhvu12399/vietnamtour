bcffc6b1c1f0d122ec8630bc873af27410fba19b
Mac <mac@Macs-MacBook-Pro.local>
2026-06-17 09:59:33 -0400

    feat: embed Sanity Studio at /studio page route

---
+++ src/sanity.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { schemaTypes } from './sanity/schema';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dummy-project-id';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+
+export default defineConfig({
+  name: 'default',
+  title: 'Vietnam Tour Admin',
+
+  projectId,
+  dataset,
+
+  basePath: '/studio',
+
+  plugins: [structureTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
